115acae11be5779833c49cea704b7f7078048853
Paul Yi <paul.yi326@gmail.com>
2014-09-21 23:39:08 -0700

    debugging

--- app.js
+++ app.js
@@ -13,17 +13,19 @@ app.use(bodyParser.urlencoded({
 }));
 
 // enable CORS
-// app.all('*', function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     next();
-// });
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 
 // api.createDummyUser();
 
 // for testing locally
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    // res.sendFile(__dirname + '/index.html');
+    api.getMessages(req, res, io);
+    
 });
 
 app.get('/getMessages', function(req, res) {
