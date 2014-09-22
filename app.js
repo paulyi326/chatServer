913b5250fe1010e82f32c70098b45814c7f71f93
Paul Yi <paul.yi326@gmail.com>
2014-09-22 00:11:15 -0700

    lksdjflkasjf

--- app.js
+++ app.js
@@ -13,23 +13,14 @@ app.use(bodyParser.urlencoded({
 }));
 
 // enable CORS
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization');
- 
-  // intercept OPTIONS method
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  }
-  else {
+app.all('*', function(req, res, next) {
+    var headers = {
+      "access-control-allow-origin": "*",
+      "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "access-control-allow-headers": "content-type, accept",
+    };
+    res.set(headers);
     next();
-  }
-};
-
-app.configure(function() {
-    app.use(allowCrossDomain);   // make sure this is is called before the router
-    app.use(app.router);      // not entirely necessary--will be automatically called with the first .get()
 });
 
 // api.createDummyUser();
