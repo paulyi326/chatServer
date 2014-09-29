92647e5386cb6eb80d571775202e848b58386930
Paul Yi <paul.yi326@gmail.com>
2014-09-28 21:41:40 -0700

    added async library to help with getFriends

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cors = require('cors');
 
 // var bodyParser = require('body-parser');
 // app.use(bodyParser.urlencoded({
-//     extended: true
+//   extended: true
 // }));
 
 // takes data the client sends to server in post request
@@ -15,84 +15,81 @@ var cors = require('cors');
 
 // for testing locally
 // app.get('/', function(req, res){
-//     res.sendFile(__dirname + '/index.html');
+//   res.sendFile(__dirname + '/index.html');
 // });
 
 app.get('/getMessages', cors(), function(req, res) {
-    api.getMessages(req, res, io);
+  api.getMessages(req, res, io);
 });
 
 app.get('/getFriends', cors(), function(req, res) {
-    api.getFriends(req, res);
+  api.getFriends(req, res);
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
+  console.log('a user connected');
 
-    socket.on('disconnect', function() {
-        console.log('user disconnected');
-    });
+  socket.on('disconnect', function() {
+      console.log('user disconnected');
+  });
 
-    // creates a room that is unique to a user
-    socket.on('join', function(userID) {
-        socket.join(userID);
-        console.log(userID + ' has joined');
+  // creates a room that is unique to a user
+  socket.on('join', function(userID) {
+    socket.join(userID);
+    console.log(userID + ' has joined');
 
-        console.log('auto room', io.sockets.adapter.rooms[socket.id]);
+    console.log('auto room', io.sockets.adapter.rooms[socket.id]);
 
-        // delete the room that's auto-generated since we dont use it
-        delete io.sockets.adapter.rooms[socket.id];
-    
-        var rooms = io.sockets.adapter.rooms;
-        var userRoom = rooms[userID];
-        console.log("all rooms", rooms);
+    // delete the room that's auto-generated since we dont use it
+    delete io.sockets.adapter.rooms[socket.id];
 
-    });
+    var rooms = io.sockets.adapter.rooms;
+    var userRoom = rooms[userID];
+    console.log("all rooms", rooms);
 
-    socket.on('chat message', function(msg) {
+  });
 
-        console.log('incoming message', msg);
+  socket.on('chat message', function(msg) {
 
-        // hash containing all the rooms
-        var rooms = io.sockets.adapter.rooms;
+    console.log('incoming message', msg);
 
-        /* PROBLEM I NEED TO CONSIDER:
-            what happens when server restarts and i send a message
-            to someone who hasn't joined a room since the server went down.
-            Then I'd be sending a message to a room that is undefined
+    // hash containing all the rooms
+    var rooms = io.sockets.adapter.rooms;
 
-            Maybe create the room, and add it to messages property. Then when
-            the other user joins, he'll just join the room that was created?
-        */
+    /* PROBLEM I NEED TO CONSIDER:
+        what happens when server restarts and i send a message
+        to someone who hasn't joined a room since the server went down.
+        Then I'd be sending a message to a room that is undefined
 
-        // create a room for user if it does not exist, due to reasons in above comment
-        if (!rooms[msg.to]) {
-            rooms[msg.to] = [];
-        }
+        Maybe create the room, and add it to messages property. Then when
+        the other user joins, he'll just join the room that was created?
+    */
 
-        // have to test this. even if i message someone who has not joined, 
-        // there shouldn't be an error
-        var userRoom = rooms[msg.to];
+    // create a room for user if it does not exist, due to reasons in above comment
+    if (!rooms[msg.to]) {
+      rooms[msg.to] = [];
+    }
 
-        // If user is currently in their room
-        // the '>= 1' is because sometimes the same user is in the room twice 
-        // but under different session ids. Idk why yet.
-        if (Object.keys(userRoom).length >= 1) {
+    // have to test this. even if i message someone who has not joined, 
+    // there shouldn't be an error
+    var userRoom = rooms[msg.to];
 
-            console.log('message was sent', msg.text)
-            io.to(msg.to).emit('chat message', msg); 
-        }
+    // If user is currently in their room
+    // the '>= 1' is because sometimes the same user is in the room twice 
+    // but under different session ids. Idk why yet.
+    if (Object.keys(userRoom).length >= 1) {
 
-        // save messages to db regardless
-        api.saveMessage(msg);
+      console.log('message was sent', msg.text)
+      io.to(msg.to).emit('chat message', msg); 
+    }
 
-        console.log('rooms after sending to someone who hasnt joined', rooms);
+    // save messages to db regardless
+    api.saveMessage(msg);
 
-
-    });
+    console.log('rooms after sending to someone who hasnt joined', rooms);
+  });
 });
 
 module.exports.app = app;
 module.exports.http = http;
-
 
--- db/api.js
+++ db/api.js
@@ -1,112 +1,168 @@
 var mongojs = require('mongojs');
 var chatUrl = process.env.chatUrl || 'chat';
 var db = mongojs(chatUrl, ['users']);
+var async = require('async');
 
 exports.getFriends = function(req, res) {
 
-    console.log('req.query', req.query);
+  console.log('req.query', req.query);
 
-    var userID = +req.query.userID;
+  var userID = +req.query.userID;
 
-    // just send back dummy data for now
+  db.users.findOne({
+    id: userID
+  }, function(err, user) {
+    if (err) {
+      console.log('error in finding user');
+      res.status(404).send('error in finding user');
+    } else {
+      if (user) {
 
-    res.send([
-        { title: 'Kobe', id: 1 },
-        { title: 'Kyrie', id: 2 },
-        { title: 'Tim', id: 3 },
-        { title: 'Jeremy', id: 4 },
-        { title: 'Manu', id: 5 },
-        { title: 'Dwight', id: 6 },
-        { title: 'paul', id: 7}
-    ]);
+        console.log('user in getFriends function', user);
+        var userFriends = user.friendIds || [];
+
+        // find all of a user's friend ids and send to client in array
+        async.map(userFriends, function(friendID, callback) {
+          console.log('friendID in async.map', friendID);
+
+          exports.findFriend(friendID, callback);
+
+        }, function(err, friends) {
+          if (err) {
+            res.status(404).send('error in finding friends');
+          } else {
+            if (friends) {
+              res.status(200).send(friends);
+            } else {
+              res.send('you have no friends');
+            }
+          }
+        });
+
+      } else {
+        console.log('user could not be found');
+        res.send('user could not be found');
+      }
+    }
+  });
+
+  // just send back dummy data for now
+  // res.send([
+  //   { title: 'Kobe', id: 1 },
+  //   { title: 'Kyrie', id: 2 },
+  //   { title: 'Tim', id: 3 },
+  //   { title: 'Jeremy', id: 4 },
+  //   { title: 'Manu', id: 5 },
+  //   { title: 'Dwight', id: 6 },
+  //   { title: 'paul', id: 7}
+  // ]);
+};
+
+// helper function used in async call to find all of a users friends
+exports.findFriend = function(friendID, callback) {
+  db.users.findOne({
+    id: friendID
+  }, function(err, user) {
+    if (err) {
+      console.log('error in finding user');
+      callback(err, null);
+    } else {
+      if (user) {
+        callback(null, user);
+      } else {
+        console.log('user not found');
+        callback(null, null);
+      }
+    }
+  });
 };
 
 exports.saveMessage = function(msg) {
-    console.log(msg);
+  console.log(msg);
 
-    // save messages to person who is receiving messages
-    db.users.findOne({
-        id: msg.to
-    }, function(err, user) {
-        if (err) {
-            console.log('error in finding user');
-        } else {
-            if (user) {
-                // if this is first msg sent to this person, msg array needs to be created
-                user.messages[msg.from] = user.messages[msg.from] || [];
+  // save messages to person who is receiving messages
+  db.users.findOne({
+    id: msg.to
+  }, function(err, user) {
+    if (err) {
+      console.log('error in finding user');
+    } else {
+      if (user) {
+        // if this is first msg sent to this person, msg array needs to be created
+        user.messages[msg.from] = user.messages[msg.from] || [];
 
-                user.messages[msg.from].push(msg.text);
-                db.users.save(user, function(err, user, lastErrObj) {
-                    console.log('message saved in db');
-                });
-            } else {
-                console.log('friend user object could not be found');
-            }
-        }
-    });
+        user.messages[msg.from].push(msg.text);
+        db.users.save(user, function(err, user, lastErrObj) {
+            console.log('message saved in db');
+        });
+      } else {
+        console.log('friend user object could not be found');
+      }
+    }
+  });
 
-    // also save messages to whoever is sending the messages
-    db.users.findOne({
-        id: msg.from
-    }, function(err, user) {
-        if (err) {
-            console.log('error in finding user');
-        } else {
-            if (user) {
-                // if this is first msg sent to this person, msg array needs to be created
-                user.messages[msg.to] = user.messages[msg.to] || [];
+  // also save messages to whoever is sending the messages
+  db.users.findOne({
+    id: msg.from
+  }, function(err, user) {
+    if (err) {
+      console.log('error in finding user');
+    } else {
+      if (user) {
+        // if this is first msg sent to this person, msg array needs to be created
+        user.messages[msg.to] = user.messages[msg.to] || [];
 
-                user.messages[msg.to].push(msg.text);
-                db.users.save(user, function(err, user, lastErrObj) {
-                    console.log('message saved in db');
-                });
-            } else {
-                console.log('current user object could not be found');
-            }
-        }
-    });
+        user.messages[msg.to].push(msg.text);
+        db.users.save(user, function(err, user, lastErrObj) {
+            console.log('message saved in db');
+        });
+      } else {
+        console.log('current user object could not be found');
+      }
+    }
+  });
 };
 
 exports.getMessages = function(req, res, io) {
 
-    console.log('inside get messages');
+  console.log('inside get messages');
 
-    // the ids come in on the req.query obj as a string for some reason
-    // '+' operator converts to number
-    var userID = +req.query.userID;
-    var friendID = +req.query.friendID;
+  // the ids come in on the req.query obj as a string for some reason
+  // '+' operator converts to number
+  var userID = +req.query.userID;
+  var friendID = +req.query.friendID;
 
-    console.log('userID', userID);
-    console.log('friendID', friendID);
+  console.log('userID', userID);
+  console.log('friendID', friendID);
 
-    // for reasons I do not understand, if there is any uncaught
-    // error in here, it shows as a CORS error on the client side
-    db.users.findOne({
-        id: userID
-    }, function(err, user) {
-        if (err) {
-            res.status(404).send({ err: err, msg: 'error trying to find user' });
-        } else {
-            if (user) {
-                console.log('current user object', user);
-                // create messages array if these two friends have never
-                // exchanged messages yet
-                user.messages[friendID] = user.messages[friendID] || [];
-                var messages = user.messages[friendID];
-                for (var i = 0; i < messages.length; i++) {
-                    var msg = {
-                        to: userID,
-                        from: friendID,
-                        text: messages[i]
-                    }
-                    io.to(userID).emit('chat message', msg);
-                }
-                res.status(200).send('messages sent. I hope you received them');
-            } else {
-                res.send('could not find the user')
-            }
+  // for reasons I do not understand, if there is any uncaught
+  // error in here, it shows as a CORS error on the client side
+  db.users.findOne({
+    id: userID
+  }, function(err, user) {
+    if (err) {
+      res.status(404).send({ err: err, msg: 'error trying to find user' });
+    } else {
+      if (user) {
+        console.log('current user object', user);
+        // create messages array if these two friends have never
+        // exchanged messages yet
+        user.messages[friendID] = user.messages[friendID] || [];
+        var messages = user.messages[friendID];
+        for (var i = 0; i < messages.length; i++) {
+          var msg = {
+            to: userID,
+            from: friendID,
+            text: messages[i]
+          }
+          io.to(userID).emit('chat message', msg);
         }
-    }); 
+        res.status(200).send('messages sent. I hope you received them');
+      } else {
+        res.send('could not find the user')
+      }
+    }
+  }); 
 };
 
 exports.createUser = function() {
